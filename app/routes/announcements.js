b87451f0921e463377fe3d4c772d0985fa285c61
Lauren Posey and Sky Rousse <sky@>
2016-08-15 13:48:05 -0700

    add route form for creating new announcements

--- app/routes/announcements.js
+++ app/routes/announcements.js
@@ -5,6 +5,11 @@ export default Ember.Route.extend({
     return this.store.findAll('announcement');
   },
   actions: {
+    save3(params) {
+      var newAnnouncement = this.store.createRecord('announcement', params);
+      newAnnouncement.save();
+      this.transitionTo('announcements');
+    },
     destroyAnnouncement(announcement) {
       announcement.destroyRecord();
       this.transitionTo('announcements');
